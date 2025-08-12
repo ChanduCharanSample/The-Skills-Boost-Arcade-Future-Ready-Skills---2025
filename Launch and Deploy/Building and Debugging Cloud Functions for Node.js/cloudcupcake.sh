ea1c09ba45b446f67da28132451fc215d5b2a56f
Pothina Chandrikarani <140737438+ChanduCharanSample@users.noreply.github.com>
2025-08-12 20:02:54 +0530

    Update cloudcupcake.sh

--- Launch and Deploy/Building and Debugging Cloud Functions for Node.js/cloudcupcake.sh
+++ Launch and Deploy/Building and Debugging Cloud Functions for Node.js/cloudcupcake.sh
@@ -14,9 +14,9 @@ RESET=$(tput sgr0)
 
 echo
 echo "${YELLOW}${BOLD}############################################${RESET}"
-echo "${YELLOW}${BOLD}#     				   	                  #${RESET}"
-echo "${YELLOW}${BOLD}#	  📢 SUBSCRIBE TO CLOUDCUPCAAKE-1217 📢  #${RESET}"
-echo "${YELLOW}${BOLD}#     				   	                  #${RESET}"
+echo "${YELLOW}${BOLD}#     				   	                   #${RESET}"
+echo "${YELLOW}${BOLD}#	  📢 SUBSCRIBE TO CLOUDCUPCAAKE-1217 📢   #${RESET}"
+echo "${YELLOW}${BOLD}#     				   	                   #${RESET}"
 echo "${YELLOW}${BOLD}############################################${RESET}"
 echo
 
